3ca4b317c5c74e3a29b0fa4e7fceecaecf8bf311
Anthony Dela Cruz <delacruztony.adc@gmail.com>
2017-11-17 14:24:58 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Switch, Link, NavLink} from 'react-router-dom'
+import "CLASS NAME" from ""
+import "CLASS NAME" from ""
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-         FUCK YOU WINDOWS
-        </p>
+        <NavLink to="1stPage">
+          <p>
+            LOG IN 
+          </p>
+        </NavLink>
+
+        <NavLink to="2ndPage">
+          <p>
+            CHAT ROOM 
+          </p>
+        </NavLink>
+
+        <Switch>
+          <Route path="/1stPage" component={"COMPONENT NAME HERE"}/>
+          <Route path="/2ndPage" component={"COMPONENT NAME HERE"}/>
+        </Switch>
       </div>
     );
   }
