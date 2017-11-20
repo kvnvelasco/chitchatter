ca922b8e2b66f59d3eb6ea4c6bb15d4cd919a5c8
Geri Escandor <escwon21@gmail.com>
2017-11-20 11:34:13 +0800

    Edited CSS
    1. Changed the color palette, from pink to charcoal & fresh green.
    2. Removed the animation from the logo.

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,33 +1,37 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
 class Home extends Component{
     render(){
+
         return(
             <div>
-                
                 <div className="logo-title">
-                    <img src="logo.png"/>
-                    <p className="app-title">ChitChatter</p>
+                    <img className="App-logo" src="https://image.ibb.co/gvqtiR/logo.png" />
+                    <p className="app-title">ChitChat</p>
                 </div>
 
                 <div className="container">
-                    <p>Join the Chatrom</p>
+                    <div className="jumbotron">
+                    <p>Join a Chatroom</p>
                     <form>
                         <div className="form-inputs">
                             <input type="text" name="username" placeholder="username"/>
                         </div>
+                        <br />
                         <div className="form-inputs">
                             <input type="text" name="chatroomname" placeholder="chatroom-name"/>
                         </div>
+                        <br />
                         <input type="submit" name="submit" value="Enter Room" onClick={(e)=>{
                             this.props.history.push('/chatroom')
                             e.preventDefault()
                         }} />
                     </form>
+                    </div>
                 </div>
-
-            </div>
+                </div>
         )
     }
 }
+
 export default Home;
